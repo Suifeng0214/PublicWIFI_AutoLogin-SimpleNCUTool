4024c0142714ac5763562ebaa50149c6875efc89
Suifeng0214 <47234216+Suifeng0214@users.noreply.github.com>
2022-03-24 13:57:02 +0800

    update

--- execute.js
+++ execute.js
@@ -1,4 +1,4 @@
-//setInterval(function(){
+function login(){
 	var account = "學號@cc.ncu.edu.tw";
 	var pwd = "NCUPortal的密碼";
 	if (document.location.host == "192.168.3.254:444"){ //Library_wireless
@@ -10,9 +10,15 @@
 		password_submitBtn.click();
 		alert("連線成功");
 	}else if (document.location.host == "wlan.cc.ncu.edu.tw"){ // TANetRoming、NCUFree
-	//https://wlan.cc.ncu.edu.tw/upload/custom/default/index.html?
 		user1.value = account;
 		password.value = pwd;
 		document.getElementsByClassName("button")[0].click();
 		alert("連線成功");
+	}else if (document.location.host == "ubnt.csie.ncu.edu.tw"){ // NCU-CSIE
+		username.value = account;
+		password.value = pwd;
+		document.getElementById("submit").click();
+		alert("連線成功");
 	}
+}
+setTimeout(login(), 200);
