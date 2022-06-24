f2512ad2fd8fb1812d11b500467edc003642cc31
Suifeng0214 <47234216+Suifeng0214@users.noreply.github.com>
2022-06-24 08:29:39 +0800

    add "NCUWL" and fixed some bugs
    - update "manifest_version" to 3
    - support "securelogin.arubanetworks.com"

--- execute.js
+++ execute.js
@@ -1,15 +1,16 @@
 function login(){
 	var account = "學號@cc.ncu.edu.tw";
 	var pwd = "NCUPortal的密碼";
-	if (document.location.host == "192.168.3.254:444"){ //Library_wireless
+	if (document.location.host == "192.168.3.254:444"){ // Library_wireless
 		document.location.href = "192.168.2.8";
 	}
-	if (document.location.host == "192.168.3.254"){ //Library_wireless
+	if (document.location.host == "192.168.3.254"){ // Library_wireless
 		password_name.value = account;
 		password_pwd.value = pwd;
 		password_submitBtn.click();
 		alert("連線成功");
-	}else if (document.location.host == "wlan.cc.ncu.edu.tw"){ // TANetRoming
+	}else if (document.location.host == "wlan.cc.ncu.edu.tw" ||
+			  document.location.host == "securelogin.arubanetworks.com"){ // TANetRoming、NCUWL
 		user1.value = account;
 		password.value = pwd;
 		document.getElementsByClassName("button")[0].click();
